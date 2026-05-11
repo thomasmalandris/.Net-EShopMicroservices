1a03854b5a67bce24587caea1808f525bf3e0c81
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-11 13:46:30 +0300

    50. Develop Create Product Endpoint with Minimal APIs and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,9 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFIle, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
 public class CreateProductEndpoint
 {
     
