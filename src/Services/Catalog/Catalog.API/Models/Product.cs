9d6712c747f9eebda4355271cffb5e562e1c7327
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-11 15:27:47 +0300

    64. Connect Postgres DB from local Catalog Microservices and send POST request

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -3,9 +3,9 @@
 public class Product
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = null!;
-    public List<string> Category { get; set; } = [];
-    public string Description { get; set; } = null!;
-    public string ImageFile { get; set; } = null!;
+    public string Name { get; set; } = default!;
+    public List<string> Category { get; set; } = new();
+    public string Description { get; set; } = default!;
+    public string ImageFile { get; set; } = default!;
     public decimal Price { get; set; }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFIle, decimal Price);
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
 
@@ -8,7 +8,7 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/products", async (CreateProductResult request, ISender sender) =>
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CreateProductCommand>();
 
