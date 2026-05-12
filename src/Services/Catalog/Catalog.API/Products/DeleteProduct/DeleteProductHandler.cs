ec780ac034304147c63de532cc4ce0911ef0b3fe
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-12 13:51:51 +0200

    87. Develop CRUD Command Validators with Fluent Validation Pipeline Behaviour

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -3,6 +3,14 @@
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool isSuccess);
 
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(command => command.id).NotEmpty().WithMessage("Product Id is required");
+    }
+}
+
 internal class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
 {
     public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -11,6 +11,18 @@ public record UpdateProductCommand(
 
 public record UpdateProductResult(bool isSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is  required");
+        RuleFor(command => command.Name)
+            .NotEmpty().WithMessage("Product Name is  required")
+            .Length(2, 150).WithMessage("Name must  be between 2 and 150 characters");
+        RuleFor(command => command.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
 internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
     public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
