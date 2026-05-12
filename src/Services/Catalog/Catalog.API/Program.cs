c4a2b62884ffb66a46eeeff28cda1542ee9fe62c
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-12 21:33:09 +0200

    92. Logging Behavior in MediatR Pipeline for Cross-cutting concerns

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviour<,>));
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
