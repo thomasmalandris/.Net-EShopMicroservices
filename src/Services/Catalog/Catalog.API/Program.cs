aa7e4678d1796b1bfb225b581ea02700f6575887
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-12 22:07:53 +0200

    98. Health Check for Catalog Microservices with PostgresDB

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container.
 var assembly = typeof(Program).Assembly;
@@ -24,11 +25,18 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database") ?? "");
+
 var app = builder.Build();
 
 //configure the HTTP request pipeline
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
