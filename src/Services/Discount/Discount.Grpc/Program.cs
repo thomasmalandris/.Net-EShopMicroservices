458fe1a7b8008061e52843fd310b3e9951f526e9
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-13 14:23:34 +0200

    155. Configure DiscountGrpc Service to Request Pipeline in Program.cs and Invoke Grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
