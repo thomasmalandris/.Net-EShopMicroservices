fcc77f2a175d8ab219cdfa69ecdafa86ac8fece4
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-13 12:19:34 +0200

    119. Implement IBasketRepository Class using Marten IDocumentSession interface

--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,3 +4,6 @@ global using Carter;
 global using MediatR;
 global using Mapster;
 global using FluentValidation;
+global using Marten;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
