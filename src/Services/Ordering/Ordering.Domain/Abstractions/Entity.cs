456743cb7e6f3e586c0d6ef1079a37e0e36a43db
LAPTOPSERVER\laptop <greekharvarddev@gmail.com>
2026-05-14 12:43:17 +0200

    191. Develop Domain Modeling with DDD for Ordering Microservices

--- src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -2,7 +2,7 @@
 
 public abstract class Entity<T> : IEntity<T>
 {
-    public T Id { get; set; }
+    public required T Id { get; set; }
     public DateTime? CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTime? LastModified { get; set; }
---
+++ src/Services/Ordering/Ordering.Domain/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Domain.Enums;
+
+public enum OrderStatus
+{
+    Draft = 1,
+    Pending = 2,
+    Complete = 3,
+    Canceled = 4
+}
